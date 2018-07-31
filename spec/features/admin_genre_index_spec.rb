586ffd7bcb24ab6d06d6cce7dff73422dd77f78f
Eliot Swank <eliotswank@gmail.com>
2018-07-31 10:06:52 -0600

    adds user story 4 test

--- spec/features/admin_genre_index_spec.rb
+++ spec/features/admin_genre_index_spec.rb
@@ -12,6 +12,23 @@ describe "Admin" do
 
       fill_in :genre_name,	with: "jazz"
     end
+
+    it 'fills out a form and creates a new genre and redirects to genre index' do
+      genre1 = Genre.create(name: "rock")
+      genre2 = Genre.create(name: "roll")
+      admin = User.create(username: 'bob', password: 'dave', role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit genres_path
+
+      fill_in :genre_name,	with: "Jazz" 
+      click_on :submit
+
+      expect(current_path).to eq(genres_path)
+      expect(page).to have_content(genre1.name)
+      expect(page).to have_content(genre2.name)
+      expect(page).to have_content("Jazz")
+    end
   end
 end
 
@@ -26,4 +43,15 @@ As an admin user,
   
 Testing requirements:
 - users should see at least 2 genres listed on the page
+
+User Story 4:
+
+As an admin user, 
+  When I visit the genre index page,
+    And I fill out the form with a new genre's name and submit that form, 
+  I return to the genre index page,
+    And I see all previous genres and the new genre I have just created.
+
+Testing requirements:
+- I should see at least 2 previous genres listed plus the new genre
 =end
