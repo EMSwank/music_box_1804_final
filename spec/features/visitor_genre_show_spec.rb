5f8dcd61b38f8c134b52ba4980faa3b876484dea
Eliot Swank <eliotswank@gmail.com>
2018-07-31 10:27:56 -0600

    adds test for story 6

---
+++ spec/features/visitor_genre_show_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "Visitor" do
+  context "visits genre show" do
+    it "sees all songs associated with that genre" do
+      genre = Genre.create(name: 'pop')
+      artist = Artist.create(name: 'Prince')
+      song1 = genre.songs.create(title: 'song1', length: 100, play_count: 10, rating: 4)
+      song2 = genre.songs.create(title: 'song2', length: 200, play_count: 50, rating: 3)
+
+      visit genre_path(genre)
+
+      expect(page).to have_content(song1.name) 
+      expect(page).to have_content(song2.name) 
+    end
+  end
+end
+
+
+=begin
+User Story 6:
+
+As a Visitor,
+  When I visit a genre show page,
+    I see all songs associated with that genre listed on the page.
+
+Testing requirements:
+- I should see at least 2 songs listed for a genre
+- Also include 1 or more songs that are NOT associated with this genre and ensure
+  they do not appear on the page
+    =end
